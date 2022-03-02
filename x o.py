af3b6eeb362d97deda078ab2b2ec1aafb95b26e1
seifeldin-sayed <96207423+seifeldin-sayed@users.noreply.github.com>
2022-03-02 22:49:14 +0200

    Update x o.py

--- x o.py
+++ x o.py
@@ -1,35 +1,16 @@
 import random
-def board_all(player,p,board_1,board_2,board_3):
-    if p=='1':
-        board_1.remove('1')
-        board_1.insert(0,player)
-    elif p=='2':
-        board_1.remove('2')
-        board_1.insert(1,player)
-    elif p=='3':
-        board_1.remove('3')
-        board_1.insert(2,player)
-    elif p=='4':
-        board_2.remove('4')
-        board_2.insert(0,player)
-    elif p=='5':
-        board_2.remove('5')
-        board_2.insert(1,player)
-    elif p=='6':
-        board_2.remove('6')
-        board_2.insert(2,player)
-    elif p=='7':
-        board_3.remove('7')
-        board_3.insert(0,player)
-    elif p=='8':
-        board_3.remove('8')
-        board_3.insert(1,player)
-    elif p=='9':
-        board_3.remove('9')
-        board_3.insert(2,player)
-    print(board_1)
-    print(board_2)
-    print(board_3)
+def board_all(player,p,board):
+    if p== '1' or p=='2' or p=='3':
+        board[0].insert(board[0].index(p),player)
+        board[0].remove(p)
+    elif p== '4' or p=='5' or p=='6':
+        board[1].insert(board[1].index(p),player)
+        board[1].remove(p)
+    else :
+        board[2].insert(board[2].index(p),player)
+        board[2].remove(p)
+    for col in range(3):
+        print( ' ' + board[col][0] +' | '+ board[col][1] + ' | ' + board[col][2])
     return(player)
 def swap_player(player):
     if player == 'X':
@@ -37,50 +18,40 @@ def swap_player(player):
     else:
         player = 'X'
     return(player)
-def check_win(player,board_1,board_2,board_3):
+def check_win(player,board):
     c=0
-    if board_1[0]==board_2[0]==board_3[0]:
+    for row in range(3):
+        if board[row][0]==board[row][1]==board[row][2]:
+            print(player + ' won')
+            c=1
+    for col in range(3):
+        if board[0][col]==board[1][col]==board[2][col]:
+            print(player + ' won')
+            c=1
+    if board[0][0]==board[1][1]==board[2][2]:
         print(player + ' won')
         c=1
-    elif board_1[1]==board_2[1]==board_3[1]:
-        print(player + ' won')
-        c=1
-    elif board_1[2]==board_2[2]==board_3[2]:
-        print(player + ' won')
-        c=1
-    elif board_1[0]==board_1[1]==board_1[2]:
-        print(player + ' won')
-        c=1
-    elif board_2[0]==board_2[1]==board_2[2]:
-        print(player + ' won')
-        c=1
-    elif board_3[0]==board_3[1]==board_3[2]:
-        print(player + ' won')
-        c=1
-    elif board_1[0]==board_2[1]==board_3[2]:
-        print(player + ' won')
-        c=1
-    elif board_1[2]==board_2[1]==board_3[0]:
+    elif board[0][2]==board[1][1]==board[2][0]:
         print(player + ' won')
         c=1
     return(c)
-def play(turn,player,board_1,board_2,board_3,bag):
+def play(turn,player,board,bag):
     print(player +' turn')
     p = input("Choose a place:")
     if p.isdigit():
         if p in bag:
-            board_all(player,p,board_1,board_2,board_3)
+            board_all(player,p,board)
             bag.remove(str(p)) 
         else:
             print("you have to enter a number from 1 to 9! ")
-            play(turn,player,board_1,board_2,board_3,bag)
+            play(turn,player,board,bag)
     else:
         print("you have to enter a number! ")
-        play(turn,player,board_1,board_2,board_3,bag)
+        play(turn,player,board,bag)
     return(player)
-def comp(player,board_1,board_2,board_3,bag):
+def comp(player,board,bag):
     p = random.choice(bag)
-    board_all(player,p,board_1,board_2,board_3)
+    board_all(player,p,board)
     bag.remove(str(p))
 def game():
     bagn=['1','2']
@@ -92,17 +63,16 @@ def game():
     while player not in bagp:
         player = input("Enter X or O:").upper()
     turn=1
-    board_1 = [ '1' , '2' , '3' ]
-    board_2 = [ '4' , '5' , '6' ]
-    board_3 = [ '7' , '8' , '9' ]
-    print(board_1)
-    print(board_2)
-    print(board_3)
+    board=[ ['1','2','3'],
+            ['4','5','6'],
+            ['7','8','9']]
+    for col in range(3):
+            print( ' ' + board[col][0] +' | '+ board[col][1] + ' | ' + board[col][2])
     bag=['1','2','3','4','5','6','7','8','9']
     if num_player== '2':
         while turn<10:
-            player=play(turn,player,board_1,board_2,board_3,bag)
-            c=check_win(player,board_1,board_2,board_3)
+            player=play(turn,player,board,bag)
+            c=check_win(player,board)
             player = swap_player(player)
             if c==1:
                 break  
@@ -111,8 +81,8 @@ def game():
                 print("it's a tie game")
     else:
         while turn<6:
-            player=play(turn,player,board_1,board_2,board_3,bag)
-            c=check_win(player,board_1,board_2,board_3)
+            player=play(turn,player,board,bag)
+            c=check_win(player,board)
             player = swap_player(player)
             if c==1:
                 break
@@ -120,8 +90,8 @@ def game():
                 print("it's a tie game") 
                 break
             print("\n")
-            comp(player,board_1,board_2,board_3,bag)
-            c=check_win(player,board_1,board_2,board_3)
+            comp(player,board,bag)
+            c=check_win(player,board)
             player = swap_player(player)
             if c==1:
                 break 
